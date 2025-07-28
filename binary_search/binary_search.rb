4bd9e15c813a38f974ab50e0b73206e55c7c909a
Abhilash A K <mailto.abhilashak@gmail.com>
2025-07-29 01:39:12 +0530

    fix: binary search: array with length two -  #2

--- binary_search/binary_search.rb
+++ binary_search/binary_search.rb
@@ -1,13 +1,27 @@
 # frozen_string_literal: true
 
 #####################################################################
-#
-#
-#
+# Given an array of numbers and a target value, this algorithm finds
+# then index of the target in the given array. If not found writes
+# 'Not found'
 #####################################################################
 def binary_search(numbers, target)
-  raise ArgumentError, 'Array cannot be empty' if numbers.empty?
-  raise ArgumentError, 'Target cannot be empty' unless target
+  raise ArgumentError, 'Provide an array of numbers' unless numbers.is_a?(Array)
+  return 'Not found' if numbers.empty?
 
-  target if numbers.length == 1
+  if numbers.length == 1
+    return numbers[0] == target ? 0 : 'Not found'
+  end
+
+  mid = (numbers.length / 2).to_i # calculate mid position
+
+  return mid if target == numbers[mid]
+
+  if target < numbers[mid]
+    return binary_search(numbers[0...mid], target)
+  elsif target > numbers[mid]
+    return binary_search(numbers[(mid + 1)..], target)
+  end
+
+  'Not found'
 end
--- binary_search/test_binary_search.rb
+++ binary_search/test_binary_search.rb
@@ -13,19 +13,23 @@ class TestBinarySearch < Minitest::Test
 
   def test_empty_array
     assert_raises ArgumentError do
-      binary_search
+      binary_search(' ', 9)
     end
     assert_raises ArgumentError do
-      binary_search([])
+      binary_search
     end
+    assert_equal 'Not found', binary_search([], 3)
   end
 
   def test_array_with_length_one
-    assert_equal 1, binary_search([1], 1)
-    assert_equal 2, binary_search([2], 2)
+    assert_equal 0, binary_search([1], 1)
+    assert_equal 0, binary_search([2], 2)
+    assert_equal 'Not found', binary_search([2], 3)
+    assert_equal 'Not found', binary_search([7], 9)
   end
 
-  def _test_array_with_length_two
-    assert_equal 2, binary_search([1, 2], 2)
+  def test_array_with_length_two
+    assert_equal 1, binary_search([1, 2], 2)
+    assert_equal 'Not found', binary_search([1, 2], 4)
   end
 end
